f376f4334ce18b6d97e42a1c89dab310f0ea3a87
Guilherme <guilherme.francisquini@hotmail.com>
2019-09-02 00:33:42 -0300

    18. Hora de Praticar - Template

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        name: 'Guilherme Francisquini',
+        age: 25,
+        img: 'C:/Users/Guilherme%20Garimpa/Pictures/16711772_1445429945491356_6763007007511248032_n.jpg',
+    },
+    methods: {
+        randonNumber: function(){
+            return (Math.random());
+        }
+    }
+})
